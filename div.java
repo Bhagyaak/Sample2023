2df8805e6ba6ec415e6dc0667e16f763c325cf3d
Bhagyaak <kavijanb@gmail.com>
2023-01-02 15:37:30 +0530

    div file added

---
+++ div.java
@@ -0,0 +1,11 @@
+class Sum{
+    public static void main(String args[])
+      
+      int a = 3;
+      int b = 8;
+      int d = 2;
+      int c = 6;
+      System.out.println(a+b);//summation
+      // ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+    
+      
