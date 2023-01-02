45bd9f17974338b5977ba6ae5fc94b090ab31921
Bhagyaak <kavijanb@gmail.com>
2023-01-02 15:27:22 +0530

    subtraction finished

---
+++ sub.java
@@ -0,0 +1,11 @@
+class Sum{
+    public static void main(String args[])
+      
+      int a = 3;
+      int b = 8;
+     
+      System.out.println(a-b);
+      // important file
+      // Subtraction finished
+      
+}
